f5f5332aff98e009876ddc9ca2fa78232e45c5f6
Yago Rigon <ya_rigon@hotmail.com>
2020-05-24 19:13:27 -0300

    Fim

--- end.js
+++ end.js
@@ -15,14 +15,14 @@ saveHighScore = e => {
   console.log("clicked the save button!");
   e.preventDefault();
   const score={
-      score: Math.floor(Math.random() * 100),
+      score: mostRecentScore,
       name: username.value
   };
   highScores.push(score);
   highScores.sort( (a,b) => b.score - a.score)
   highScores.splice(5);
-
+  console.log(highScores);
   localStorage.setItem('highScores', JSON.stringify(highScores));
   window.location.assign("/");
-  console.log(highScores);
+  
 };
